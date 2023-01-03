acf998f07fda059d3859ffb5ca1e9004de51daa9
Hanni <ngynngchn@gmail.com>
2023-01-03 14:14:01 +0100

    initial commit

---
+++ lev1_7/assets/js/main.js
@@ -0,0 +1,20 @@
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjDubai = "828m";
+console.log(burjDubai);
+
+let fullName = "Jan Schmidt";
+console.log(fullName);
+
+const eifelTower = "324m";
+console.log(eifelTower);
+
+let camelCase = "camelCase";
+console.log(camelCase);
+
+let kursStatus = true;
+console.log(kursStatus);
